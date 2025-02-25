5d4b8fcc7f245bf76b948c100b13be6c65dc7359
Zoë Langhoff-Weinstein <zlweinstein@wisc.edu>
2025-02-25 15:27:03 -0600

    adding data cleaning file

---
+++ data-cleaning.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib as plt
+import numpy as np
+import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+df = pd.read_stata("/Users/zoeweinstein/Desktop/spring-25/soc-693/programming/py/")
